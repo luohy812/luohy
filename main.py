2005c1d8d44b68751087954659ba4f7b631bfc97
luohy812 <2310247011@email.szu.edu.cn>
2025-03-17 00:42:00 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from train.train_for_multiGPU import train_new_GPUs_ours
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+
+
+    train_new_GPUs_ours()
+
+
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+
